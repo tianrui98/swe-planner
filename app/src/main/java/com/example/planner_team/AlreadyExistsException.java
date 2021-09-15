03dcf488c65aa7a0f90dcb903ddd46d84d5d080a
Tian Rui <zhutianrui1998@gmail.com>
2021-09-15 16:41:03 +0800

    added exception classes

---
+++ app/src/main/java/com/example/planner_team/AlreadyExistsException.java
@@ -0,0 +1,6 @@
+package com.example.planner_team;
+
+public class AlreadyExistsException extends Exception{
+       public AlreadyExistsException() {
+        super("Object already exists!");}
+}
---
+++ app/src/main/java/com/example/planner_team/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.planner_team;
+
+public class NotFoundException extends Exception{
+    public NotFoundException() {
+        super("Object not found!");
+    }
+}
