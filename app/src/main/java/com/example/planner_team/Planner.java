ff367e16028fc2e7513bd2893b4bd94f462d6dd0
Sewen Thy <sewenthy@gmail.com>
2021-09-17 21:42:38 +0800

    normalized input

--- app/src/main/java/com/example/planner_team/Planner.java
+++ app/src/main/java/com/example/planner_team/Planner.java
@@ -87,7 +87,7 @@ public class Planner {
           Document doc = db.parse(data);
 
           // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-          //doc.getDocumentElement().normalize();
+          doc.getDocumentElement().normalize();
           NodeList boards = doc.getElementsByTagName("board");
           for (int i = 0; i < boards.getLength(); i++)
 	      this.addBoard(Board.parseBoardElement(boards.get(i), doc));
